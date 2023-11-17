23b8efd657ba4b6f61d0faebe07c883c07b0c8ae
feliperyder <147994564+feliperyder@users.noreply.github.com>
2023-11-17 19:38:55 +0000

    .js section developed - Function to prompt user for password options

--- script.js
+++ script.js
@@ -1,102 +1,27 @@
 // Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
-
-// Array of numeric characters to be included in password
+var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
+var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 // Function to prompt user for password options
 function getPasswordOptions() {
   // Prompt for password length
+  var passwordLength = parseInt(prompt('Enter how many characters you want your password to be (between 8 and 128).'));
   // Validate password length
+  while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert('Invalid password length (Must be between 8 and 128 characters).');
+    passwordLength = parseInt(prompt('Enter how many characters you want your password to be (between 8 and 128).'));
 }
 
 // Prompt for character types
-  // Validate at least one character type is selected
+var includeLowercase=confirm('Include lowercase characters?');
+var includeUppercase=confirm('Include uppercase characters?');
+var includeNumeric=confirm('Include numeric characters?');
+var includeSpecial=confirm('Include special characters?');
 
+  // Validate at least one character type is selected
+}
 // Store user choices in an options object
 
 // Function for getting a random element from an array
